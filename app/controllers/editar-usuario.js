ee5eb91292649284c0765a460655b387f9668960
jdperezv <jdperezv@unal.edu.co>
2018-04-24 18:53:22 -0500

    Controlador para campo incompleto y mal digitado

--- app/controllers/editar-usuario.js
+++ app/controllers/editar-usuario.js
@@ -1,4 +1,22 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+	actions:{
+		wrongField() {
+			var code=this.get('code');
+			var a=typeof code;
+			if (code==null||code=="") {
+				alert("Es necesario que especifique la cédula del usuario que desea editar");
+				this.set("prueba",a);
+           	}
+           	else if ( isNaN(code)){
+           		alert("Datos no válidos, verifique la información registrada");
+           		this.set("prueba",a);
+           	}
+           	else{
+           		this.set("prueba",a);
+           	}
+
+	}
+	}
 });
