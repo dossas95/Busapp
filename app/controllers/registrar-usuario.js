e48027953dc18e7bd57027ba6e9f404c44d40ee6
DESKTOP-R0NGCAK\ASUS <dossas@unal.edu.co>
2018-04-24 19:21:03 -0500

    Resgistro de usuario sin base de datos.

--- app/controllers/registrar-usuario.js
+++ app/controllers/registrar-usuario.js
@@ -1,4 +1,38 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-});
+  actions: {
+    registrar() {
+
+      const nombre = document.getElementById('nombre').value;
+      const grupoSanguineo = document.getElementById('gs').value;
+      const cedula = document.getElementById('cc').value;
+      const rh = document.getElementById('rh').value;
+      const contrasena = document.getElementById('pass').value;
+      const numeroContacto = document.getElementById('nc').value;
+      const fechaNacimiento = document.getElementById('fn').value;
+      const fotografia = document.getElementById('foto').value;
+      const direccion = document.getElementById('dire').value;
+      const telefono = document.getElementById('tel').value;
+
+      if (nombre == 'Nombre completo' || grupoSanguineo == 'Grupo Sanguíneo (A,B,O,AB)' || cedula == 'Cédula' || rh == 'Rh (+ , -)' ||
+          contrasena == 'Contraseña' || numeroContacto == 'Número de contacto' || fechaNacimiento == 'Fecha de nacimiento' ||
+          direccion == 'Dirección' || telefono == 'Teléfono'){
+
+            alert("Es necesario que llene todos los campos del formulario");
+      }
+      else if(grupoSanguineo != 'A' || grupoSanguineo != 'B' || grupoSanguineo != 'O' || grupoSanguineo != 'AB' ||
+              rh != '+' || rh != '-' || isNaN(cedula) || isNaN(numeroContacto) || isNaN(telefono)){
+
+            alert("Datos no válidos, verifique la información registrada");
+            location.href="/registrar-usuario";
+      }
+
+
+
+
+
+
+    }
+
+}});
