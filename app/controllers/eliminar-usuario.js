2a4927adc9e90b449ca6c2a14f442513b6ab19f8
Unknown <jtapiase@unal.edu.co>
2018-04-24 22:52:34 -0500

    adding more feautures to template

--- app/controllers/eliminar-usuario.js
+++ app/controllers/eliminar-usuario.js
@@ -1,4 +1,20 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    actions:{
+        buscar:function(){
+            const cedula = document.getElementById('cc').value;
+           
+            if(isNaN(cedula)){
+                alert("Datos equivocados, debe ingresar una cedula valida");
+                location.href="/eliminar-usuario";
+            }else{
+                document.getElementById('segundo').hidden = false;
+                document.getElementById('primero').hidden = true;
+            }
+        }
+        
+    }
+
+
 });
